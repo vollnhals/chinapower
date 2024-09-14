2a677cdb535f9480fa8f4b2380e12f906c7df289
Lion Vollnhals <lion@giantmonkey.de>
2024-09-14 21:45:22 +0200

    collect data full script

--- bin/collect_data.sh
+++ bin/collect_data.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-curl -u admin:admin 10.10.100.254/status.html | grep -E "^var .* = .*;" | sed 's/^var //' | sed 's/ = /: /' | sed 's/;/,/' | sed '$s/,//' | sed '1s/^/{\n/' | sed '$s/$/\n}/' | sed -E 's/^([^:]+):/"\1":/g' | curl -v -H 'Content-Type: application/json' --data-binary @- localhost:3001/data
+
+# use Wechselrichter, network 1
+wpa_cli -i wlan0 set_network 0 priority 0
+wpa_cli -i wlan0 set_network 1 priority 2
+wpa_cli -i wlan0 reassociate
+
+curl -u admin:admin 10.10.100.254/status.html | grep -E "^var .* = .*;" | sed 's/^var //' | sed 's/ = /: /' | sed 's/;/,/' | sed '$s/,//' | sed '1s/^/{\n/' | sed '$s/$/\n}/' | sed -E 's/^([^:]+):/"\1":/g' > /tmp/powerdata
+
+# use fritzbox, network 0
+wpa_cli -i wlan0 set_network 0 priority 2
+wpa_cli -i wlan0 set_network 1 priority 0
+wpa_cli -i wlan0 reassociate
+
+curl -v -H 'Content-Type: application/json' --data-binary /tmp/powerdata chinapower.fly.dev/data
